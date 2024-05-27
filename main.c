09867f7ecd814194b6db02d0e438b6a843f9d8e6
Pedro <143135026+Psaa70@users.noreply.github.com>
2024-05-27 22:51:12 +0200

    Update main.c
    Added checks and logic for leap years

--- main.c
+++ main.c
@@ -8,12 +8,35 @@ int main() {
 
     printf("Enter day: ");
     scanf("%d", &q);
+    while (q<1||q>31){  //check if the year is correct
+        printf("Enter a correct day: ");
+        scanf("%d", &q);
+    }
 
     printf("Enter month: ");
     scanf("%d", &m);
+    while (m<1||m>12){ //check if the month is correct
+        printf("Enter a correct month: ");
+        scanf("%d", &m);
+    }
+    if (m==2 && q>29){  //special condition for february
+        while (q<1||q>29){
+            printf("Enter a day <= 29 for february: ");
+            scanf("%d", &q);
+        }
+    }
 
     printf("Enter year: ");
     scanf("%d", &year);
+    while (year<0) { //check if the year is correct
+        printf("Enter a correct year: ");
+        scanf("%d", &year);
+    }
+    while (m==2 && q==29 && ((year%4!=0&&year%100==0)||(year % 400 != 0))) { //if the year is not a leap year
+        printf("%d is not a leap year, insert another year: ", year);
+        printf("\n* Leap years are years that are exactly divisible by 4, except for years that are exactly divisible by 100, but these years are leap years if they are exactly divisible by 400\n");
+        scanf("%d", &year);
+    }
 
     if (m < 3) {
         m += 12;  //following Zeller's Congruence, if the months are January or February,
@@ -24,6 +47,8 @@ int main() {
     j = year / 100;  //obtaining the century
 
     h = (q+(13*(m+1))/5+k+k/4+j/4+5*j)%7;
+
+    printf("The day %d/%d/%d was ", q, m, year);
 
     switch (h) {
         case 0:
